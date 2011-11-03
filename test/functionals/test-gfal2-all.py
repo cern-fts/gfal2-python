bd9928b2fe45033460b6d0a7bb4a864098fd700e
Adrien Devresse <adevress@cern.ch>
2011-11-03 17:22:51 +0000

    -add new test for getxattr

--- test/functionals/test-gfal2-all.py
+++ test/functionals/test-gfal2-all.py
@@ -7,6 +7,7 @@ from test_gfal2_mkrmdir import *
 from test_gfal2_listdir import *
 from test_gfal2_rename import *
 from test_gfal2_link import *
+from test_gfal2_xattr import *
 
 
 if __name__ == '__main__':
