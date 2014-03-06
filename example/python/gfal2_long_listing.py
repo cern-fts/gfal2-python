26889c84faecdbc82cfb6cbc33562f3d21ac9328
Adrien Devresse <adevress@cern.ch>
2014-03-06 12:20:40 +0000

    - add gfal2 long listing example

---
+++ example/python/gfal2_long_listing.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+import sys
+import gfal2
+
+
+if(len(sys.argv) < 2):
+	print("Usage %s [URL]"%(sys.argv[0]))
+	exit(1)
+
+ctxt = gfal2.creat_context()
+dirp = ctxt.opendir(sys.argv[1])
+
+while(True):
+	(dirent, stat) = dirp.readpp()
+	if(dirent is None):
+		break;
+	print("filename %s, filesize: %d, filerights 0%o"%(dirent.d_name, stat.st_size, stat.st_mode))
+
+
+	
+
