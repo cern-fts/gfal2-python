70b6e9536cf73bbb03b15fac197e234fa84f82f2
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-10-04 14:27:45 +0200

    Polish unit test file

--- test/unit/creat_instance_all.py
+++ test/unit/creat_instance_all.py
@@ -1,33 +1,31 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-import sys
 import gfal2
-import time
 
 import unittest
 
 class unit_test_instance(unittest.TestCase):
 
+    def test_creat_and_delete_instance(self):
+        gfal2.set_verbose(gfal2.verbose_level.debug)
+        context = gfal2.creat_context()
+        del context
 
-        def test_creat_and_delete_instance(self):
-            gfal2.set_verbose(gfal2.verbose_level.debug)
-            context = gfal2.creat_context()
-            del context
+    def test_creat_and_delete_params(self):
+        gfal2.set_verbose(gfal2.verbose_level.debug)
+        context = gfal2.creat_context()
+        t = context.transfer_parameters()
+        self.assertGreater(t.timeout, 0)
+        t.timeout = 10
+        self.assertEqual(t.timeout, 10)
 
-        def test_creat_and_delete_params(self):
-            gfal2.set_verbose(gfal2.verbose_level.debug)
-            context = gfal2.creat_context()
-            t = context.transfer_parameters()
-            self.assertTrue(t.timeout > 0 )
-            t.timeout = 10
-            self.assertTrue(t.timeout == 10 )
-            self.assertTrue(t.checksum_check == False)
-            t.checksum_check = True
-            self.assertTrue(t.checksum_check == True)
-            self.assertTrue(t.src_spacetoken == "")
-            t.src_spacetoken = "TOKENDTEAM"
-            self.assertTrue(t.src_spacetoken == "TOKENDTEAM")
+        self.assertEqual(t.checksum_check, False)
+        t.checksum_check = True
+        self.assertEqual(t.checksum_check, True)
 
+        self.assertEqual(t.src_spacetoken, "")
+        t.src_spacetoken = "TOKENDTEAM"
+        self.assertEqual(t.src_spacetoken, "TOKENDTEAM")
 
 
 if __name__ == '__main__':
