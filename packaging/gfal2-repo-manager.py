7006731fd86d8aac62bb4bc65ea22fe54432bfd4
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2021-11-27 19:12:23 +0100

    CI: Extract release version from tag when building tag RPMs.
    The following patterns are recognized:
    - ends with number (e.g.: "-1")
    - ends with rc (e.g.: "-rc1")
    
    When no release pattern is detected from the tag, then the release version from the spec file will be used

--- packaging/gfal2-repo-manager.py
+++ packaging/gfal2-repo-manager.py
@@ -90,7 +90,7 @@ def construct_location(platform, arch, filename):
     return "{0}/{1}/{2}".format(platform, arch, filename)
 
 def is_tag(ref):
-    return (re.compile("""^(v)(\d+)\.(\d+)\.(\d+)$""").match(ref) != None or
+    return (re.compile("""^(v)(\d+)\.(\d+)\.(\d+)(-(rc)?(\d+))?$""").match(ref) != None or
            re.compile("""^(v)(\d+)\.(\d+)$""").match(ref) != None)
 
 def mkdir_p(path):
