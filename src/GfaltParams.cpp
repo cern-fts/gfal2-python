51765d699168ba04c4a63282b8c538bcae6ccfdd
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2023-09-06 17:39:35 +0200

    Use the renamed Gfal2 "gfalt_copy_get_bytes_transferred" function

--- src/GfaltParams.cpp
+++ src/GfaltParams.cpp
@@ -46,7 +46,7 @@ static void monitor_callback_wrapper(gfalt_transfer_status_t h, const char* src,
     if (callbacks->monitor_callback) {
         size_t avg     = gfalt_copy_get_average_baudrate(h, NULL);
         size_t inst    = gfalt_copy_get_instant_baudrate(h, NULL);
-        size_t trans   = gfalt_copy_get_bytes_transfered(h, NULL);
+        size_t trans   = gfalt_copy_get_bytes_transferred(h, NULL);
         time_t elapsed = gfalt_copy_get_elapsed_time(h, NULL);
 
         boost::python::call<void>(callbacks->monitor_callback.ptr(), src, dst, avg, inst, trans, elapsed);
